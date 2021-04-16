e2ea1cbf6b5de909945fadde15eafd5ab70cea9f
xobotyi <xog3@yandex.ru>
2021-04-17 00:51:42 +0300

    feat: add main file and reexport new hooks

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { useFirstMountState } from './useFirstMountState';
+export { useMountEffect } from './useMountEffect';
+export { useUpdateEffect } from './useUpdateEffect';
+export { useUnmountEffect } from './useUnmountEffect';
