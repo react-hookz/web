1a34605333c7554e12625434b0e37da4ec04bed7
Joe Duncko <JoeDuncko@users.noreply.github.com>
2021-04-18 19:37:49 -0400

    docs: update useMountEffect and useUpdateEffect docs (#1)
    * docs: update useMountEffect and useUpdateEffect docs
    
    * Lint

--- src/useMountEffect.ts
+++ src/useMountEffect.ts
@@ -3,7 +3,7 @@ import { EffectCallback, useEffect } from 'react';
 /**
  * Run effect only when component is first mounted.
  *
- * @param effect effector to run on nmount
+ * @param effect effector to run on mount
  */
 export function useMountEffect(effect: EffectCallback): void {
   useEffect(() => {
