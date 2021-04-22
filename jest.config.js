b535b035dad32a413a6a596fdae9a048cc9fcef6
xobotyi <xog3@yandex.ru>
2021-04-22 13:04:31 +0300

    chore: make SSR tests run in pure node environment

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,13 @@ module.exports = {
     {
       displayName: 'dom',
       preset: 'ts-jest',
+      testEnvironment: 'jsdom',
       testMatch: ['<rootDir>/tests/dom/**/*.test.[jt]s?(x)'],
     },
     {
       displayName: 'ssr',
       preset: 'ts-jest',
+      testEnvironment: 'node',
       testMatch: ['<rootDir>/tests/ssr/**/*.test.[jt]s?(x)'],
     },
   ],
