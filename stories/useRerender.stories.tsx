cbd7b5f50bfc5556220e1283bd152dfd8e7bd5bc
Joe Duncko <JoeDuncko@users.noreply.github.com>
2021-04-25 21:47:12 -0400

    docs: useRerender docs

---
+++ stories/useRerender.stories.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useRef } from 'react';
+import { useRerender } from '../src';
+
+export const Example: React.FC<{ initialCount: number }> = () => {
+  const count = useRef(0);
+  const rerender = useRerender();
+
+  count.current += 1;
+
+  return (
+    <div>
+      <div>This component has rendered {count.current} times</div>
+      <button
+        onClick={() => {
+          rerender();
+        }}>
+        Rerender
+      </button>
+    </div>
+  );
+};
