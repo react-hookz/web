357923d8d2112a5fa10d55649614c8e989ca4fe1
xobotyi <xog3@yandex.ru>
2021-05-05 21:37:07 +0300

    docs: fix jsdoc placement, it was stripped in JS output

--- src/useMediatedState.ts
+++ src/useMediatedState.ts
@@ -2,9 +2,6 @@ import { useCallback, useRef } from 'react';
 import { resolveHookState } from './util/resolveHookState';
 import { useSafeState } from './useSafeState';
 
-/**
- * Like `useState`, but every value set is passed through a mediator function.
- */
 export function useMediatedState<S>(
   initialState?: S | (() => S)
 ): [S, (value: S | (() => S)) => void];
@@ -12,6 +9,10 @@ export function useMediatedState<S, R>(
   initialState?: S | (() => S),
   mediator?: (state: R) => S
 ): [S, (value: R | ((prevState: S) => S)) => void];
+
+/**
+ * Like `useState`, but every value set is passed through a mediator function.
+ */
 export function useMediatedState<S, R>(
   initialState?: S | (() => S),
   mediator?: (state: R) => S
--- src/useSafeState.ts
+++ src/useSafeState.ts
@@ -1,16 +1,17 @@
 import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { useIsMounted } from './useIsMounted';
 
-/**
- * Like `useState` but its state setter is guarded against sets on unmounted component.
- *
- * @param initialState
- */
 export function useSafeState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
 export function useSafeState<S = undefined>(): [
   S | undefined,
   Dispatch<SetStateAction<S | undefined>>
 ];
+
+/**
+ * Like `useState` but its state setter is guarded against sets on unmounted component.
+ *
+ * @param initialState
+ */
 export function useSafeState<S>(initialState?: S | (() => S)): [S, Dispatch<SetStateAction<S>>] {
   const [state, setState] = useState(initialState);
   const isMounted = useIsMounted();
