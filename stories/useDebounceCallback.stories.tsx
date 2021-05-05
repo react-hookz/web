4756dfdf534598e37277ff7125d45855983802e8
xobotyi <xog3@yandex.ru>
2021-05-05 10:09:01 +0300

    feat: new hook useDebounceCallback

---
+++ stories/useDebounceCallback.stories.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { useDebounceCallback } from '../src';
+
+export const Example: React.FC = () => {
+  const [state, setState] = useState('');
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = useDebounceCallback(
+    (ev) => {
+      setState(ev.target.value);
+    },
+    500,
+    []
+  );
+
+  return (
+    <div>
+      <div>Below state will update within 500ms after last change</div>
+      <br />
+      <div>The input`s value is: {state}</div>
+      <input type="text" onChange={handleChange} />
+    </div>
+  );
+};
