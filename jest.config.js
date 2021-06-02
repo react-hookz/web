23f3e15c5ce864ca1516621fd689deae46c66a5c
Anton Zinovyev <xog3@yandex.ru>
2021-06-02 22:30:55 +0300

    test: add tests against distribution bundle (#104)
    fix: #103

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,26 @@ module.exports = {
       testEnvironment: 'node',
       testMatch: ['<rootDir>/src/**/__tests__/ssr.[jt]s?(x)'],
     },
+
+    // needed for output bundle testing
+    {
+      displayName: 'dom-cjs',
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/src/**/__tests__/dom.[jt]s?(x)'],
+      moduleNameMapper: {
+        '^../..$': '<rootDir>/cjs',
+      },
+    },
+    {
+      displayName: 'dom-package',
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/src/**/__tests__/dom.[jt]s?(x)'],
+      moduleNameMapper: {
+        '^../..$': '<rootDir>',
+      },
+    },
   ],
   coverageDirectory: './coverage',
   collectCoverage: false,
