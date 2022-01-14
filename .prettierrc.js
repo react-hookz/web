13102e1666d1052620bb92a4045e386651e564c4
Kyle Holmberg <inbox@kylemh.com>
2022-01-14 19:04:45 +0700

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  tabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  endOfLine: 'lf',
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+};
