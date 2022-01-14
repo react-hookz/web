be19333cad4616221f47b2ab1e86ae2cf0921f6b
Joe Duncko <JoeDuncko@users.noreply.github.com>
2022-01-13 20:11:19 -0500

    Add prompt for others to contribute examples

--- src/useDeepCompareEffect/__docs__/example.stories.tsx
+++ src/useDeepCompareEffect/__docs__/example.stories.tsx
@@ -4,5 +4,12 @@ import { useDeepCompareEffect } from '../..';
 export const Example: React.FC = () => {
   useDeepCompareEffect(() => {}, []);
 
-  return <div>Im unable to imagine worthy example 😭😭</div>;
+  return (
+    <div>
+      <p>
+        We don&apos;t have an example for useDeepCompareEffect yet. Want to{' '}
+        <a href="https://github.com/react-hookz/web/issues/582">contribute one</a>?
+      </p>
+    </div>
+  );
 };
