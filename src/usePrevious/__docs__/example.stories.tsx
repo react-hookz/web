47010d1edcd81230ca39fd822d99a24756a2d474
Kyle Holmberg <kylemh.email@gmail.com>
2022-01-16 23:02:52 +0700

    docs(usePrevious): Update documentation and tests with added context of usePreviousDistinct existing
    -

--- src/usePrevious/__docs__/example.stories.tsx
+++ src/usePrevious/__docs__/example.stories.tsx
@@ -2,25 +2,30 @@ import React, { useState } from 'react';
 import { usePrevious } from '../..';
 
 export const Example: React.FC = () => {
-  const [val, setVal] = useState(0);
-  const prevVal = usePrevious(val);
+  const [value, setValue] = useState(0);
+  const [unrelatedValue, setUnrelatedValue] = useState(0);
+  const previousValue = usePrevious(value);
+
+  const increment = () => setValue((v) => v + 1);
+  const decrement = () => setValue((v) => v - 1);
+  const triggerUnrelatedRerender = () => setUnrelatedValue((v) => v + 1);
 
   return (
     <div>
-      <span>Current value: {val}</span>{' '}
-      <button
-        onClick={() => {
-          setVal((v) => v + 1);
-        }}>
-        increment
-      </button>{' '}
-      <button
-        onClick={() => {
-          setVal((v) => v - 1);
-        }}>
-        decrement
-      </button>
-      <div>Previous value: &quot;{prevVal ?? 'undefined'}&quot;</div>
+      <span>Current value: {value}</span>
+
+      <div style={{ margin: '1rem 0' }}>
+        <div style={{ marginBottom: '0.5rem' }}>
+          <button onClick={increment}>increment</button>
+          <button onClick={decrement}>decrement</button>
+        </div>
+
+        <button onClick={triggerUnrelatedRerender}>
+          trigger unrelated rerender - {unrelatedValue}
+        </button>
+      </div>
+
+      <div>Previous value: &quot;{previousValue ?? 'undefined'}&quot;</div>
     </div>
   );
 };
