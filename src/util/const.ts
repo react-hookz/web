df480034410c13f9d28f10ea15897bc899be0cf8
Kyle Holmberg <kylemh.email@gmail.com>
2022-01-17 17:48:49 +0700

    docs(usePreviousDistinct): add comments to silly util fn
    -

--- src/util/const.ts
+++ src/util/const.ts
@@ -7,6 +7,10 @@ export const isBrowser =
   typeof navigator !== 'undefined' &&
   typeof document !== 'undefined';
 
+/**
+ * You should only be reaching for this function when you're attempting to prevent multiple
+ * redefinitions of the same function. In-place strict equality checks are more performant.
+ */
 export const isStrictEqual: TPredicate = <T>(prev: T, next: any) => prev === next;
 
 export const truthyAndArrayPredicate: IConditionsPredicate = (conditions): boolean =>
