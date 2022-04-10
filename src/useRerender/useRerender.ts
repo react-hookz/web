409a21f73d3250ad08e5a46da59cc2db88f29390
Anton Zinovyev <xog3@yandex.ru>
2022-04-10 22:30:16 +0300

    fix: make `useRerender` operate integer increment instead of bool switch (#711)
    fix: make `useRerender` operate integer increment instead of bool switch
    
    Fix #691

--- src/useRerender/useRerender.ts
+++ src/useRerender/useRerender.ts
@@ -1,13 +1,13 @@
 import { useCallback } from 'react';
 import { useSafeState } from '..';
 
-const stateChanger = (state: boolean) => !state;
+const stateChanger = (state: number) => state + (1 % Number.MAX_SAFE_INTEGER);
 
 /**
  * Return callback function that re-renders component.
  */
 export function useRerender(): () => void {
-  const [, setState] = useSafeState(false);
+  const [, setState] = useSafeState(0);
 
   return useCallback(() => {
     setState(stateChanger);
