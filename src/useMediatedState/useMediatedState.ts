f1d86790eaeba5fa08bbb6670098a6bdaea0037c
xobotyi <xog3@yandex.ru>
2022-05-12 01:17:01 +0300

    fix(`useMediatedState`): make initial state be passed through mediator
    Also improve typing

--- src/useMediatedState/useMediatedState.ts
+++ src/useMediatedState/useMediatedState.ts
@@ -1,11 +1,15 @@
 import { Dispatch, useCallback } from 'react';
-import { IInitialState, INextState, resolveHookState } from '../util/resolveHookState';
 import { useSafeState, useSyncedRef } from '..';
+import { IInitialState, INextState, resolveHookState } from '../util/resolveHookState';
 
+export function useMediatedState<State = undefined>(): [
+  State | undefined,
+  Dispatch<INextState<State | undefined>>
+];
 export function useMediatedState<State>(
-  initialState?: IInitialState<State>
-): [State, Dispatch<State>];
-export function useMediatedState<State, RawState>(
+  initialState: IInitialState<State>
+): [State, Dispatch<INextState<State>>];
+export function useMediatedState<State, RawState = State>(
   initialState: IInitialState<State>,
   mediator?: (state: RawState) => State
 ): [State, Dispatch<INextState<RawState, State>>];
@@ -13,19 +17,23 @@ export function useMediatedState<State, RawState>(
 /**
  * Like `useState`, but every value set is passed through a mediator function.
  */
-export function useMediatedState<State, RawState>(
+export function useMediatedState<State, RawState = State>(
   initialState?: IInitialState<State>,
-  mediator?: (state: RawState | undefined) => State
+  mediator?: (state: RawState | State | undefined) => State
 ): [State | undefined, Dispatch<INextState<RawState, State | undefined>>] {
-  const [state, setState] = useSafeState(initialState);
+  const [state, setState] = useSafeState(() => {
+    return mediator ? mediator(resolveHookState(initialState)) : initialState;
+  });
   const mediatorRef = useSyncedRef(mediator);
 
   return [
-    state,
+    state as State,
     useCallback((value) => {
       if (mediatorRef.current) {
         setState((prevState) =>
-          mediatorRef.current?.(resolveHookState<RawState, State | undefined>(value, prevState))
+          mediatorRef.current?.(
+            resolveHookState<RawState, State | undefined>(value, prevState as State)
+          )
         );
       } else {
         setState(value as unknown as State);
