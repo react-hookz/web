5fc735ad2b582394c913ffc2440495213d1dff2d
Konrad Linkowski <linkowski.konrad97@gmail.com>
2022-06-22 21:20:20 +0200

    fix: apply correct order of operations in `useRerender` (#850)
    First add one to state and then get reminder of max safe integer

--- src/useRerender/useRerender.ts
+++ src/useRerender/useRerender.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 import { useSafeState } from '..';
 
-const stateChanger = (state: number) => state + (1 % Number.MAX_SAFE_INTEGER);
+const stateChanger = (state: number) => (state + 1) % Number.MAX_SAFE_INTEGER;
 
 /**
  * Return callback function that re-renders component.
