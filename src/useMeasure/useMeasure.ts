161c18dd2f16e9e4f5164b6bb78799df184dde13
Arttu Olli <60509537+ArttuOll@users.noreply.github.com>
2022-11-12 18:22:31 +0200

    fix(useMeasure): only expose with and height properties (#954)
    Only the width and height of the observed elements content rect are now exposed because the other
    properties were not considered relevant for this hook. In addition they introduced potential
    footguns. See #954 for more.
    
    BREAKING CHANGE: Only width and height are now exposed from the element instead of the whole DOMRect
    #951

--- src/useMeasure/useMeasure.ts
+++ src/useMeasure/useMeasure.ts
@@ -7,6 +7,11 @@ import { useHookableRef } from '../useHookableRef/useHookableRef';
 import { useRafCallback } from '../useRafCallback/useRafCallback';
 import { useSafeState } from '../useSafeState/useSafeState';
 
+export interface Measures {
+  width: number;
+  height: number;
+}
+
 /**
  * Uses ResizeObserver to track element dimensions and re-render component when they change.
  *
@@ -14,7 +19,7 @@ import { useSafeState } from '../useSafeState/useSafeState';
  */
 export function useMeasure<T extends Element>(
   enabled = true
-): [DOMRectReadOnly | undefined, MutableRefObject<T | null>] {
+): [Measures | undefined, MutableRefObject<T | null>] {
   const [element, setElement] = useSafeState<T | null>(null);
   const elementRef = useHookableRef<T | null>(null, (v) => {
     setElement(v);
@@ -22,12 +27,12 @@ export function useMeasure<T extends Element>(
     return v;
   });
 
-  const [rect, setRect] = useSafeState<DOMRectReadOnly>();
+  const [measures, setMeasures] = useSafeState<Measures>();
   const [observerHandler] = useRafCallback<UseResizeObserverCallback>((entry) =>
-    setRect(entry.contentRect)
+    setMeasures({ width: entry.contentRect.width, height: entry.contentRect.height })
   );
 
   useResizeObserver(element, observerHandler, enabled);
 
-  return [rect, elementRef];
+  return [measures, elementRef];
 }
