bd2d6f4ea46ba4d2c964f9ac1546d855fa861c7a
jpwallace22 <jpwallace22@gmail.com>
2022-12-08 21:16:09 -0800

    Include story.mdx

--- src/useQueue/useQueue.ts
+++ src/useQueue/useQueue.ts
@@ -1,13 +1,33 @@
 import { useState } from 'react';
 
 export interface QueueMethods<T> {
+  /**
+   * Function for adding an item to to the queue
+   * @param item The item to be added
+   */
   add: (item: T) => void;
+  /**
+   * Function to remove and return an item from the queue
+   */
   remove: () => T;
+  /**
+   * The first item in current queue
+   */
   first: T;
+  /**
+   * The last item in current queue
+   */
   last: T;
+  /**
+   * The current size of te queue
+   */
   size: number;
 }
 
+/**
+ * A react state hook implements simple first-in first-out (FIFO) queue.
+ * @param initialValue an array for the initial value of the queue
+ */
 export const useQueue = <T>(initialValue: T[] = []): QueueMethods<T> => {
   const [state, set] = useState(initialValue);
   return {
