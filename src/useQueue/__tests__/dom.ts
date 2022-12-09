87f0b2cba099d517d023df119d7ed00577e6eec3
jpwallace22 <jpwallace22@gmail.com>
2022-12-09 08:31:20 -0800

    add dom tests

--- src/useQueue/__tests__/dom.ts
+++ src/useQueue/__tests__/dom.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks/dom';
+import { act, renderHook } from '@testing-library/react-hooks/dom';
 import { useQueue } from '../..';
 
 describe('useQueue', () => {
@@ -9,5 +9,42 @@ describe('useQueue', () => {
   it('should render', () => {
     const { result } = renderHook(() => useQueue());
     expect(result.error).toBeUndefined();
+  });
+
+  it('should accept an initial value', () => {
+    const { result } = renderHook(() => useQueue([0, 1, 2, 3]));
+    expect(result.current.first).toBe(0);
+  });
+});
+
+describe('remove', () => {
+  it('should remove the first value', () => {
+    const { result } = renderHook(() => useQueue([0, 1, 2, 3]));
+
+    act(() => {
+      const removed = result.current.remove();
+      expect(removed).toBe(0);
+    });
+
+    expect(result.current.first).toBe(1);
+  });
+});
+
+describe('size', () => {
+  it('should return the length', () => {
+    const { result } = renderHook(() => useQueue([0, 1, 2, 3]));
+    expect(result.current.size).toBe(4);
+  });
+});
+
+describe('add', () => {
+  it('should add a value to the end', () => {
+    const { result } = renderHook(() => useQueue([0, 1, 2, 3]));
+
+    act(() => {
+      result.current.add(4);
+    });
+
+    expect(result.current.last).toBe(4);
   });
 });
