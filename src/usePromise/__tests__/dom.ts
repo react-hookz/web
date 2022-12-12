bec4251e225811f6a9062e46ad11f7fd6442777c
jpwallace22 <jpwallace22@gmail.com>
2022-12-12 11:04:55 -0800

    finish writing tests (that I can figure out)

--- src/usePromise/__tests__/dom.ts
+++ src/usePromise/__tests__/dom.ts
@@ -1,9 +1,20 @@
 import { renderHook } from '@testing-library/react-hooks/dom';
 import { usePromise } from '../..';
 
+jest.useFakeTimers();
+
 describe('usePromise', () => {
-  const promise = new Promise<number>((resolve) => {
+  const resolves = new Promise<number>((resolve, reject) => {
     resolve(1);
+    reject(Error);
+  });
+
+  const rejects = new Promise<number>((resolve, reject) => {
+    // eslint-disable-next-line no-constant-condition
+    if (1 < 0) {
+      resolve(1);
+    }
+    reject(Error);
   });
 
   it('should be defined', () => {
@@ -15,12 +26,26 @@ describe('usePromise', () => {
     expect(result.error).toBeUndefined();
   });
 
-  it('should not return if unmounted', async () => {
-    const { result, unmount } = renderHook(() => usePromise());
-    unmount();
-    const res = await result.current(promise);
+  it('should return resolved value', async () => {
+    const { result } = renderHook(() => usePromise());
 
-    expect(result.error).toBeUndefined();
-    expect(res).toBeUndefined();
+    await expect(result.current(resolves)).resolves.toBe(1);
   });
+
+  it('should return rejection value', async () => {
+    const { result } = renderHook(() => usePromise());
+
+    expect.assertions(1);
+    await expect(result.current(rejects)).rejects.toBe(Error);
+  });
+
+  // it('should not return if unmounted', async () => {
+  //   const { result, unmount } = renderHook(() => usePromise());
+  //   unmount();
+
+  //   expect(result.error).toBeUndefined();
+  //   await expect(result.current(resolves)).resolves.toBeUndefined();
+  // });
 });
+
+jest.clearAllTimers();
