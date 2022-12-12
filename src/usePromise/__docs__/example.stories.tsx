294c0e72a7a85ff182b0ca447d5c5d8153fe2fde
jpwallace22 <jpwallace22@gmail.com>
2022-12-11 21:16:25 -0800

    add example story

--- src/usePromise/__docs__/example.stories.tsx
+++ src/usePromise/__docs__/example.stories.tsx
@@ -1,6 +1,31 @@
 import * as React from 'react';
-// import { usePromise } from '../..';
+import { useState, useEffect } from 'react';
+import { usePromise } from '../..';
 
 export const Example: React.FC = () => {
-  return null;
+  const mounted = usePromise();
+  const [value, setValue] = useState<string>();
+
+  const myPromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('foo');
+    }, 300);
+  });
+
+  useEffect(() => {
+    (async () => {
+      const res = await mounted(myPromise);
+
+      if (typeof res === 'string') {
+        // This line will not execute if this component gets unmounted.
+        setValue(res);
+      }
+    })();
+  });
+
+  return (
+    <div>
+      <button>{value}</button>
+    </div>
+  );
 };
--- src/usePromise/usePromise.ts
+++ src/usePromise/usePromise.ts
@@ -1,19 +1,18 @@
 import { useCallback } from 'react';
 import { useIsMounted } from '../useIsMounted/useIsMounted';
+import { noop } from '../util/const';
 
 export type UsePromise = () => <T>(promise: Promise<T>) => Promise<T>;
 
 export const usePromise: UsePromise = () => {
   const isMounted = useIsMounted();
+
   return useCallback(
     <T>(promise: Promise<T>) =>
       new Promise<T>((resolve, reject) => {
-        const onResolve = (resolution: T) => {
-          return isMounted() && resolve(resolution);
-        };
-        const onReject = (rejection: T) => {
-          return isMounted() && reject(rejection);
-        };
+        const onResolve = (resolution: T) => (isMounted() ? resolve(resolution) : noop);
+
+        const onReject = (rejection: unknown) => (isMounted() ? reject(rejection) : noop);
 
         // eslint-disable-next-line promise/catch-or-return
         promise.then(onResolve, onReject);
