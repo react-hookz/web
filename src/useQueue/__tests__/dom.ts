4866d2421f4345ba5d96a462d8e6b57e75fd1442
jpwallace22 <jpwallace22@gmail.com>
2022-12-21 19:09:22 -0800

    Write unit tests for 'lilo' variant and `items` property

--- src/useQueue/__tests__/dom.ts
+++ src/useQueue/__tests__/dom.ts
@@ -13,7 +13,7 @@ describe('useQueue', () => {
 
   it('should accept an initial value', () => {
     const { result } = renderHook(() => useQueue([0, 1, 2, 3]));
-    expect(result.current.first).toBe(0);
+    expect(result.current.items).toStrictEqual([0, 1, 2, 3]);
   });
 
   it('should remove the first value', () => {
@@ -27,6 +27,17 @@ describe('useQueue', () => {
     expect(result.current.first).toBe(1);
   });
 
+  it('should remove the last value', () => {
+    const { result } = renderHook(() => useQueue([0, 1, 2, 3], 'lilo'));
+
+    act(() => {
+      const removed = result.current.remove();
+      expect(removed).toBe(3);
+    });
+
+    expect(result.current.first).toBe(2);
+  });
+
   it('should return the length', () => {
     const { result } = renderHook(() => useQueue([0, 1, 2, 3]));
     expect(result.current.size).toBe(4);
@@ -34,6 +45,16 @@ describe('useQueue', () => {
 
   it('should add a value to the end', () => {
     const { result } = renderHook(() => useQueue([0, 1, 2, 3]));
+
+    act(() => {
+      result.current.add(4);
+    });
+
+    expect(result.current.last).toBe(4);
+  });
+
+  it('should add a value to the beginning', () => {
+    const { result } = renderHook(() => useQueue([0, 1, 2, 3], 'lilo'));
 
     act(() => {
       result.current.add(4);
