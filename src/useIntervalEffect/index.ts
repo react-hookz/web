5b5ce34186c1440b220316cadce2bff8de0dba9c
Arttu Olli <60509537+ArttuOll@users.noreply.github.com>
2023-01-15 17:22:17 +0200

    docs(useIntervalEffect): Improve docs

--- src/useIntervalEffect/index.ts
+++ src/useIntervalEffect/index.ts
@@ -2,12 +2,11 @@ import { useEffect } from 'react';
 import { useSyncedRef } from '../useSyncedRef';
 
 /**
- * Like `setInterval` but in form of react hook.
+ * Like `setInterval` but in the form of a React hook.
  *
- * @param callback Callback to be called within interval.
- * @param ms Interval delay in milliseconds, `undefined` disables the interval.
- * Keep in mind, that changing this parameter will re-set interval, meaning
- * that it will be set as new after the change.
+ * @param callback Function to call within the interval.
+ * @param ms Delay passed to the underlying `setInterval`. If set to `undefined`, the interval will
+ * be cancelled. Keep in mind, that changing this parameter will reset the interval.
  */
 export function useIntervalEffect(callback: () => void, ms?: number): void {
   const cbRef = useSyncedRef(callback);
