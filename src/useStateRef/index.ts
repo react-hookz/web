f833f4a18022da0d6a4fd9fb0c774d464d3c1e01
Michał Tarasiuk <tarasiuk.michal03@gmail.com>
2023-01-28 21:12:13 +0100

    chore: update docs

--- src/useStateRef/index.ts
+++ src/useStateRef/index.ts
@@ -5,7 +5,8 @@ import { useSyncedRef } from '../useSyncedRef';
 /**
  * Hook with callback which provide access to DOM node reference.
  *
- * @param refCallback Function which call on mount and unmount of component.
+ * @param refCallback Function which call on mount and unmount of component
+ * @returns return value of `refCallback` and handler
  */
 export const useStateRef = <Node extends HTMLElement | null, StateRef>(
   refCallback: (node: Node) => StateRef
