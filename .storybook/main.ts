d7329df70b8af6d54411f5ec02aaa78a0ccbcdc4
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-04-05 12:40:25 +0300

    Run migration script provided by Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,21 @@
 import { StorybookConfig } from '@storybook/react-webpack5';
-
 const config: StorybookConfig = {
   stories: ['../src/**/__docs__/*.mdx', '../src/**/__docs__/*.tsx'],
-  addons: [
-    '@storybook/addon-postcss',
-    '@storybook/addon-links',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        backgrounds: false,
-      },
-    },
-  ],
+  addons: ['@storybook/addon-postcss', '@storybook/addon-links', {
+    name: '@storybook/addon-essentials',
+    options: {
+      backgrounds: false
+    }
+  }, '@storybook/addon-mdx-gfm'],
   staticDirs: ['./public'],
   framework: {
     name: '@storybook/react-webpack5',
-    options: { fastRefresh: true },
+    options: {
+      fastRefresh: true
+    }
   },
+  docs: {
+    autodocs: true
+  }
 };
-
 export default config;
