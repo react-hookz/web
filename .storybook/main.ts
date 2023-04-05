5dab5ea13be44748e890128d702165e5ef3cff21
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-04-05 12:29:18 +0300

    Migrate main.js to new format

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
   stories: ['../src/**/__docs__/*.mdx', '../src/**/__docs__/*.tsx'],
   addons: [
     '@storybook/addon-postcss',
@@ -11,14 +13,10 @@ module.exports = {
     },
   ],
   staticDirs: ['./public'],
-  managerWebpack: (config, options) => {
-    // ToDo: maybe enable cache mack when storybook is fixed
-    options.cache.set = () => Promise.resolve(); // eslint-disable-line @typescript-eslint/no-unsafe-member-access
-
-    return config; // eslint-disable-line @typescript-eslint/no-unsafe-return
-  },
   framework: {
     name: '@storybook/react-webpack5',
     options: { fastRefresh: true },
   },
 };
+
+export default config;
