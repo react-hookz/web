939c85be3ddf0846d9bd8a9b02606999afc49263
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-04-05 12:26:25 +0300

    Migrate preview.js to new format

--- .storybook/preview.js
+++
@@ -1,4 +0,0 @@
-export const parameters = {
-  layout: 'centered',
-  viewMode: 'docs',
-};
---
+++ .storybook/preview.ts
@@ -0,0 +1,10 @@
+import { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    layout: 'centered',
+    viewMode: 'docs',
+  },
+};
+
+export default preview;
