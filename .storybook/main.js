044c2f6c2a1dfeaa57d78195e26e9024a168c902
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2023-04-05 12:11:49 +0300

    Migrate main.js to use the new framework field

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   stories: ['../src/**/__docs__/*.mdx', '../src/**/__docs__/*.tsx'],
   addons: [
     '@storybook/addon-postcss',
@@ -13,14 +10,15 @@ module.exports = {
       },
     },
   ],
-  reactOptions: {
-    fastRefresh: true,
-  },
   staticDirs: ['./public'],
   managerWebpack: (config, options) => {
     // ToDo: maybe enable cache mack when storybook is fixed
     options.cache.set = () => Promise.resolve(); // eslint-disable-line @typescript-eslint/no-unsafe-member-access
 
     return config; // eslint-disable-line @typescript-eslint/no-unsafe-return
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: { fastRefresh: true },
   },
 };
