1848011e0879cde3b9c1009e6e45c49d8c12363e
xobotyi <xog3@yandex.ru>
2023-09-18 09:29:59 +0200

    test: refactor renderHookServer

--- src/__tests__/ssr-render-hook.tsx
+++ src/__tests__/ssr-render-hook.tsx
@@ -1,50 +1,52 @@
-import type { ReactNode } from 'react';
+import type { PropsWithChildren } from 'react';
 import * as React from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { renderToString } from 'react-dom/server';
 import { act } from 'react-dom/test-utils';
 
+type HookWrapper = (props: PropsWithChildren) => JSX.Element;
+
+type RenderHookOptions = {
+	wrapper?: HookWrapper;
+};
+
+type HookHarnessProps<Hook extends () => any> = {
+	readonly useHook: Hook;
+	readonly setResult: (result: ReturnType<Hook>) => void;
+};
+
+function HookHarness<Hook extends () => any>({ useHook, setResult }: HookHarnessProps<Hook>) {
+	setResult(useHook());
+
+	return null;
+}
+
 export const renderHookServer = <Hook extends () => any>(
 	useHook: Hook,
-	{
-		wrapper: Wrapper,
-	}: {
-		wrapper?: ({ children }: { children: ReactNode }) => JSX.Element;
-	} = {}
+	{ wrapper: Wrapper }: RenderHookOptions = {}
 ): { result: { current: ReturnType<Hook> }; hydrate: () => void } => {
-	// Store hook return value
-	const results: Array<ReturnType<Hook>> = [];
-	const result = {
-		get current() {
-			return results.slice(-1)[0];
-		},
-	};
+	// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+	const result = { current: undefined } as { current: ReturnType<Hook> };
+
 	const setValue = (value: ReturnType<Hook>) => {
-		results.push(value);
+		result.current = value;
 	};
 
-	function Component({ useHook }: { useHook: Hook }) {
-		setValue(useHook());
-		return null;
+	let harness = <HookHarness useHook={useHook} setResult={setValue} />;
+
+	if (Wrapper) {
+		harness = <Wrapper>{harness}</Wrapper>;
 	}
 
-	const component = Wrapper ? (
-		<Wrapper>
-			<Component useHook={useHook} />
-		</Wrapper>
-	) : (
-		<Component useHook={useHook} />
-	);
-
 	// Render hook on server
-	const serverOutput = renderToString(component);
+	const serverOutput = renderToString(harness);
 
 	// Render hook on client
 	const hydrate = () => {
 		const root = document.createElement('div');
 		root.innerHTML = serverOutput;
 		act(() => {
-			hydrateRoot(root, component);
+			hydrateRoot(root, harness);
 		});
 	};
 
