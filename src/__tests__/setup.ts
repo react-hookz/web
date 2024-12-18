53011cffdeff32e95d4dc46cd1a7077d6aa4cf25
xobotyi <xog3@yandex.ru>
2024-12-18 02:54:45 +0100

    chore: move tests files higher

--- src/__tests__/setup.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Here goes any setup code that should be performed before actual tests and/or code imports.
- */
-
-import {setupNavigatorVibrate} from './navigator.vibrate.js';
-
-setupNavigatorVibrate();
